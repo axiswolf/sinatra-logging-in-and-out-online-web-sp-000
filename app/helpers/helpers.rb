79e6c104011e3ea35f199b1a490a42b5b890c8f6
Karen V <karenvo@usc.edu>
2019-11-09 07:06:22 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,9 @@
 class Helpers
 
   def self.current_user(session_hash)
+    #should accept the session hash as an argument. This method should use the
+    #user_id from the session hash to find the user in the database and return
+    #that user.
     @user = User.find(session_hash[:user_id])
   end
 
