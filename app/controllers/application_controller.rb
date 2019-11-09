9b9617d083b9ff49a9776d0646691eddb694e965
Karen V <karenvo@usc.edu>
2019-11-09 07:20:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     if Helpers.is_logged_in?(session)
-      redirect to '/account'
+      erb :account
     else
       erb :error
     end
